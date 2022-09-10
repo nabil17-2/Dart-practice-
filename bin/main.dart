cdb8feca342d0eeece38f0f0f92d13d156a97d94
nabil17-2 <nabilhasanprince2@gmail.com>
2022-09-11 00:59:44 +0600

    Recheck

---
+++ bin/main.dart
@@ -0,0 +1,69 @@
+void main() {
+  // print("hello dart");
+
+//  # variable name and rules
+
+  // var name = "Sonam";
+  // var f_name = "rahul";
+  // var address1 = "Dhaka";
+  // var $money = 100;
+
+  // ## invalid variable
+  // var 1person = "sumit";
+  // var for = "male";
+
+  // printing variable
+  // print(name);
+  // print(f_name);
+  // print(address1);
+  // print($money);
+
+  // Built-in-datatype
+
+  int cart_item = 10;
+  print(cart_item);
+
+  double price = 10.23;
+  print(price);
+
+  String city = "Dhaka";
+  print(city);
+
+  bool is_completed = true;
+  print(is_completed);
+
+  bool is_login = false;
+  print(is_login);
+
+  // var quantity = 100;
+  // print(quantity);
+  // print(quantity.runtimeType);
+
+  // var quantity = "10KG";
+  // print(quantity);
+  // print(quantity.runtimeType);
+
+  // var quantity = true;
+  // print(quantity);
+  // print(quantity.runtimeType);
+
+  var quantity = 100;
+  print(quantity);
+  var cost = 100.50;
+  print(cost);
+  var gender = "Male";
+  print(gender);
+  var is_full = true;
+  print(is_full);
+
+  // #Check Data Type
+  print(cart_item.runtimeType);
+  print(price.runtimeType);
+  print(city.runtimeType);
+  print(is_completed.runtimeType);
+  print(is_login.runtimeType);
+  print(quantity.runtimeType);
+  print(cost.runtimeType);
+  print(gender.runtimeType);
+  print(is_full.runtimeType);
+}
